5ddbb90b78a3cf373549581a61469c7e1132bbae
ogh010 <ogh010@naver.com>
2021-12-26 23:03:20 +0900

    라인업포유 퍼블 수정

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
         proxy:{
             '/be':{
                 //프록시요청을 보낼 주소.
-                target: 'http://3.34.129.142:25123', //test 서버 
+                target: 'http://3.34.129.142:25123/', //test 서버 
                 changeOrigin: true,
                 ws: false,
             },
